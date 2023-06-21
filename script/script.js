6a2361eebfb7853efa63cc138371ad4da1b510b6
THIAGO DE SOUZA LIMA <114366742+a08t9608at@users.noreply.github.com>
2023-06-21 15:29:12 -0300

    login/principal
    completo terminado

---
+++ script/script.js
@@ -0,0 +1,15 @@
+function logar()  {
+    var login = document.getElementById('login').value;
+    var senha = document.getElementById('senha').value;
+   
+    if(login == "admin" && senha == "admin"){
+   
+     alert('sucesso');    
+     location.href = "../principal.html";
+
+    } else{
+        alert('Usuarios ou senha incoretos')
+    }
+
+
+ }
